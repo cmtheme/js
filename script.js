a77b2e2a8b211e5e44b980b46336c14598d3d260
cmtheme <abub1986@gmail.com>
2024-07-10 06:35:33 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var floatingIcon = document.createElement('img');
-    floatingIcon.src = 'icon.png'; // Ensure 'icon.png' is in the same directory as 'index.html'
-    floatingIcon.alt = 'Floating Icon';
-    floatingIcon.className = 'floating-icon';
-    floatingIcon.id = 'floatingIcon';
-    document.body.appendChild(floatingIcon);
-
-    var modal = document.createElement('div');
-    modal.className = 'modal';
-    modal.id = 'myModal';
-    modal.innerHTML = '' +
-        '<div class="modal-content">' +
-            '<h2>AKTIFKAN FITUR!!!</h2>' +
-            '<p>NOTE! SETTING BET TERLEBIH DAHULU</p>' +
-            '<div class="checklist">' +
-                '<input type="checkbox" id="check">' +
-                '<label for="check"><h3>AUTO SCATTER HITAM</h3></label>' +
-            '</div>' +
-            '<button class="button" id="startStopButton">' +
-                '<img src="https://i.ibb.co/WDf3MqL/start.webp" alt="START" id="startStopImage">' +
-            '</button>' +
-        '</div>';
-    document.body.appendChild(modal);
-
     var startStopButton = document.getElementById('startStopButton');
     var startStopImage = document.getElementById('startStopImage');
+    var floatingIcon = document.getElementById('floatingIcon');
+    var modal = document.getElementById('myModal');
 
     var isDraggingIcon = false, isDraggingModal = false, offsetX, offsetY, isModalOpen = false;
 
